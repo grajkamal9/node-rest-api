7725ebe7f39f59ee6a368bfd6674867847853739
grajkamal9 <grajkamal9@gmail.com>
2019-04-02 12:17:56 +0530

    Initial Commit

---
+++ routes.config.js
@@ -0,0 +1,7 @@
+
+exports.routesConfig = function (router) {
+	// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+	router.get('/', function(req, res) {
+	    res.json({ message: 'hooray! welcome to our api!' });   
+	});
+};
